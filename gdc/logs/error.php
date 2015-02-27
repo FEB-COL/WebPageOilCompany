e2a529b1729ef20afed568077e37ef5bdd5712ae
Fabian Bohorquez <fabianbohor@hotmail.com>
2015-02-27 09:39:06 -0500

    retiro de folder gdc

--- gdc/logs/error.php
+++
@@ -1,7 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2012-10-27 20:37:03 UTC
-#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
-
-#Fields: date	time	priority	clientip	category	message
-2012-10-27	20:37:03	INFO	186.29.159.203	Joomla FAILURE: 	Contraseña inválida
